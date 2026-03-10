ded26449406599acd884e184bea8d9b911f94778
Amoako Benedict Acheampong <160603063+primeyloa@users.noreply.github.com>
2026-03-10 10:15:44 +0000

    Modify pin_extractor to compute secret code from poem
    Updated pin_extractor function to determine pin based on the ith letter of the ith line.

--- pin_extractor.py
+++ pin_extractor.py
@@ -1,10 +1,19 @@
+"""This is a function that takes some poem, for which you shall
+determine the pin based on the ith letter of the ith line"""
 def pin_extractor(poem):
     secret_code = ''
     lines = poem.split('\n')
     for line_index, line in enumerate(lines):
         print(line_index, line)
         words = line.split()
+        
+        if line_index > len(words[line_index]):
+            secret_code = secret_code + words[line_index][0]
+        else:
+            secret_code = secret_code + words[line_index][line_index]
+        # print(secret_code)
         print(words)
+    print(secret_code)
 
 poem = """Stars and the moon
 shine in the sky
@@ -12,3 +21,4 @@ white and bright
 until the end of the night"""
 
 pin_extractor(poem)
+
